699a4b3f3ca71c7d1235e5fbc26f449b237e3925
4s3ti <4s3ti@4s3ti.net>
2024-01-18 20:02:14 +0100

    Basic funcionality
    Add build script
    Add workflow
    Add gitignore
    Update README

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # docker-nextcloud-full
-Nextcloud docker image containing all dependencies (full): https://github.com/nextcloud/docker/tree/master/.examples/dockerfiles/full
+Just a simple pipeline to build a Nextcloud docker image containing all dependencies (full): https://github.com/nextcloud/docker/tree/master/.examples/dockerfiles/full
+
+There is no intention to make any changes to the container, 
+It simply pulls the latest version of the repository, and builds the Dockerfiles provided by the Nextcloud team. 
+
+## Versioning
+
+Follows similar tagging and versioning scheme as the official repository.
+
+examples:
+
+`latest`, `apache`, `28.0.0`
+`fpm-alpine`, `28.0.0-fpm-alpine`
+`fpm`, `28.0.0-fpm`
+
---
+++ build.sh
@@ -0,0 +1,58 @@
+#!/bin/bash -e 
+
+if [[ -d docker ]]; then 
+  echo "docker exists, cleaning up..." 
+  rm -rf docker
+fi
+
+baseRegistry="4s3ti/nextcloud-full"
+
+git clone https://github.com/nextcloud/docker.git 
+version=$(curl -s https://api.github.com/repos/nextcloud/server/releases/latest | jq -r '.name')
+
+function buildApache() {
+  apache="${baseRegistry}:apache"
+  latest="${baseRegistry}:latest"
+  versionOnly="${baseRegistry}:${version#v}"
+  apacheVersion="${baseRegistry}:${version#v}-apache"
+
+  cd docker/.examples/dockerfiles/full/apache || exit 0
+  docker build -t "$apache" -t "$latest" -t "$versionOnly" -t "$apacheVersion" .
+
+  for tag in $apache $latest $version $apacheVersion; do
+    docker push "$tag"
+  done
+}
+
+function buildFpmAlpine() {
+  fpmAlpine="${baseRegistry}:fpm-alpine"
+  fpmAlpineVersion="${baseRegistry}:${version#v}-fpm-alpine"
+
+  cd ../fpm-alpine || exit 0
+  docker build -t "$fpmAlpine" -t "$fpmAlpineVersion" .
+
+  for tag in $fpmAlpine $fpmAlpineVersion; do
+    docker push "$tag"
+  done
+}
+
+function buildFPM() {
+  fpm="${baseRegistry}:fpm"
+  fpmVersion="${baseRegistry}:${version#v}-fpm"
+
+  cd ../fpm-alpine || exit 0
+  docker build -t "$fpm" -t "$fpmVersion" .
+
+  for tag in $fpm $fpmVersion; do
+    docker push "$tag"
+  done
+
+}
+
+function main() {
+  buildApache
+  buildFpmAlpine
+  buildFPM
+}
+
+main
